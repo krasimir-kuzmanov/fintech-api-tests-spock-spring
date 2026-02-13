f9bba50e590d2f7723882aefe221f9a124e87178
Krasimir Kuzmanov <krasimir.kuzmanov@gmail.com>
2026-02-13 13:44:37 +0200

    feat: Add LoginNegativeSpec (#3)

---
+++ src/test/groovy/com/example/fintech/spock/specs/BaseApiSpec.groovy
@@ -0,0 +1,31 @@
+package com.example.fintech.spock.specs
+
+import com.example.fintech.spock.client.AuthClient
+import com.example.fintech.spock.client.TestResetClient
+import com.example.fintech.spock.config.TestConfig
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
+import spock.lang.Specification
+
+@ContextConfiguration(classes = TestConfig)
+@TestPropertySource(locations = 'classpath:application-test.properties')
+abstract class BaseApiSpec extends Specification {
+
+  protected static final String USERNAME_PREFIX = 'spock_'
+  protected static final String DEFAULT_PASSWORD = 'password'
+
+  @Autowired
+  protected TestResetClient resetClient
+
+  @Autowired
+  protected AuthClient authClient
+
+  def setup() {
+    resetClient.reset()
+  }
+
+  protected static String newUsername() {
+    return USERNAME_PREFIX + UUID.randomUUID()
+  }
+}
